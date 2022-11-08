aff85b0c9bbc0030b642a347dae743429c6f7ab4
Hello1457 <117754167+Hello1457@users.noreply.github.com>
2022-11-08 16:50:29 +0000

    Update make-plot.R

--- git-usage/src/make-plot.R
+++ git-usage/src/make-plot.R
@@ -26,3 +26,8 @@ ggsave(filename = "data/git-usage.png",
 sink(file = "out/regression-summary.txt")
 summary(lm(percentage ~ year, data = sods_data))
 sink()
+
+
+sink(file = "out/package-versions.txt")
+sessionInfo()
+sink()
